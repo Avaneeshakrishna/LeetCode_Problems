5f7c4bfcc15d0e6f6a96c3792323674963279dec
Avaneeshakrishna <36323856+Avaneeshakrishna@users.noreply.github.com>
2020-07-21 14:14:21 +0530

    Create Contains_Duplicate.java

---
+++ Contains_Duplicate.java
@@ -0,0 +1,14 @@
+class Solution {                                                 // java class
+    public boolean containsDuplicate(int[] nums) {               // method definition
+        HashSet <Integer> set = new HashSet <Integer> ();        // Create object of HashSet
+        
+        for (int x : nums){                                      // loop for the iteration
+            if (set.contains(x))                                 // contains method checks if x is present is the set
+                return true;                                     // return answer
+            else
+                set.add(x);                                      // adding element x to the set
+        }
+            
+        return false;                                            // return ans
+    }
+}
