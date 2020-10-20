a8fed00d432779c6ff2f4419e1500937ac202d73
Avaneeshakrishna <36323856+Avaneeshakrishna@users.noreply.github.com>
2020-10-20 12:20:49 +0530

    Create Sum of Two Integers.java

---
+++ Sum of Two Integers.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int getSum(int a, int b) {
+        while(b != 0){
+        int answer = a ^ b;                 // XOR operation
+        int carry = (a & b) << 1;           // AND operation & left shift operation by 1
+        a = answer;
+        b = carry;
+        }
+        return a;
+    }
+}
